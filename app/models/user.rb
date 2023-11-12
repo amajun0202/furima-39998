d569771e78a548e6c686d4ca9154d6a463f13d1b
amajun0202 <chihirokento8888@gmail.com>
2023-11-12 14:54:03 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname,           presence: true
-         validates :email,              presence: true, uniqueness: true, format: { with: /\A\S+@\S+\z/, message: "は@を含む入力をしてください" }
-         validates :encrypted_password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/, message: "は半角英字と数字の両方を含む必要があります" }
+         validates :password,           presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/, message: "は半角英字と数字の両方を含む必要があります" }
          validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "は全角文字のみで入力してください" }
          validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "は全角文字のみで入力してください" }
          validates :first_name_kana,    presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナのみで入力してください" }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,15 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+  
+  context '新規登録できる場合' do
+
+   it '全ての項目が入力されていれば登録できる' do
+    expect(@user).to be_valid
+   end
+  end
+
+  context '新規登録できない場合' do
 
     it 'nicknameが空では登録できない' do
       @user.nickname = ''
@@ -104,6 +113,28 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Birthdate can't be blank")
     end
     
+    it '英字のみのパスワードでは登録できない' do
+      @user.password = 'aaaaaa'
+      @user.password_confirmation = 'aaaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password は半角英字と数字の両方を含む必要があります')
+    end
+
+    it '数字のみのパスワードでは登録できない' do
+      @user.password = '111111'
+      @user.password_confirmation = '111111'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password は半角英字と数字の両方を含む必要があります')
+    end
+
+    it '全角文字を含むパスワードでは登録できない' do
+      @user.password = 'ああああああ'
+      @user.password_confirmation = 'ああああああ'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password は半角英字と数字の両方を含む必要があります')
+    end
+
+  end
   end
 end
 
