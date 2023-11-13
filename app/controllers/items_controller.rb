c88e0b257ce91653def1ce8811f71a7237cfd077
amajun0202 <chihirokento8888@gmail.com>
2023-11-14 02:45:32 +0900

    パラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, ).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :category_id, :condition_id, :shipping_cost_id, :origin_region_id, :shipping_day_id, :item_name, :description, :cost ).merge(user_id: current_user.id)
   end
 
   end
