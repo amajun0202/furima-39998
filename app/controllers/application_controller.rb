25504e0792c4aef11e4aa01011011c2fb25af502
amajun0202 <chihirokento8888@gmail.com>
2023-11-15 02:11:14 +0900

    一覧機能設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_active_storage_url_options
 
   
 
@@ -10,6 +11,15 @@ class ApplicationController < ActionController::Base
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
      username == ENV["BASIC_AUTH_USER3"] && password == ENV["BASIC_AUTH_PASSWORD3"]
+    end
+  end
+
+  
+  def set_active_storage_url_options
+    if Rails.version.to_i >= 6
+      ActiveStorage::Current.url_options = request.env
+    else
+      ActiveStorage::Current.url_options = request.url_options
     end
   end
 
