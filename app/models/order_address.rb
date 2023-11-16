1b450627a6c38033a48a3dd63805b61dae8b87af
amajun0202 <chihirokento8888@gmail.com>
2023-11-16 17:56:42 +0900

    Update order_address.rb

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,8 +5,8 @@ class OrderAddress
   with_options presence: true do
   validates :postal_code,       format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :origin_region_id,  numericality: {other_than: 1, message: "can't be blank"}
-  validates :city,             
-  validates :street_number,    
+  validates :city         
+  validates :street_number  
   validates :phone_number,      numericality: { only_integer: true }, length: { in: 10..11 }
   validates :user_id          
   validates :item_id          
